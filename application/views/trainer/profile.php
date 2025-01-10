2fef1255bd0ef9631346156035a29b4915472f08
Christopher John Gamo <christopherjohngamo@gmail.com>
2025-01-10 23:03:48 +0800

    update

--- application/views/trainer/profile.php
+++ application/views/trainer/profile.php
@@ -95,7 +95,7 @@
             </div>
             <?php 
             
-            if (!isset($trainer['photo']) && empty($trainer['photo'])) {
+            if (!isset($trainer['photo']) || empty($trainer['photo'])) {
                 $trainer['photo'] = base_url() . '/assets/template/dist/img/avatar5.png';
             }
     
