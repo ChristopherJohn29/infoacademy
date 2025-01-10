278ef884aad253a5b2dcc5044d4110da987a26ee
Christopher John Gamo <christopherjohngamo@gmail.com>
2025-01-11 00:01:08 +0800

    update

--- application/views/trainer/profile.php
+++ application/views/trainer/profile.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+.<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -101,7 +101,6 @@
                 $photo = base_url('uploads/' . $trainer['photo']);
             }
     
-    
             ?>
          
 
@@ -154,127 +153,134 @@
     <!-- /.navbar -->
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container">
                 <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0 text-dark"> My Dashboard</h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item active">Home</li>
-                        </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
+                    <div class="col-sm-12">
+                        <h1 class="m-0 text-dark">My Profile</h1>
+                    </div>
+                </div>
+            </div>
         </div>
-        <!-- /.content-header -->
-        <!-- Main content -->
+
         <div class="content">
             <div class="container">
-                <div class="row" style="display:block; text-align: right; margin:2px;">
-                    <a class="btn btn-success btn-sm col-sm-2" href="#" style="margin:10px; margin-left:0px;" data-toggle="modal" data-target="#updateProfileModal">Update Profile</a>
-                </div>
-                <div class="modal fade" id="updateProfileModal" tabindex="-1" role="dialog" aria-labelledby="updateProfileModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-lg" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="updateProfileModalLabel">Update Profile</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <!-- User Information Form -->
-                                <form id="updateProfileForm">
-                                    <div class="form-group">
-                                        <label for="profilePhoto">Profile Photo</label>
-                                        <input type="file" class="form-control-file" id="profilePhoto" name="photo">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="firstName">First Name</label>
-                                        <input type="text" class="form-control" id="firstName" name="first_name" value="<?= $user['first_name'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="middleName">Middle Name</label>
-                                        <input type="text" class="form-control" id="middleName" name="middle_name" value="<?= $user['middle_name'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="lastName">Last Name</label>
-                                        <input type="text" class="form-control" id="lastName" name="last_name" value="<?= $user['last_name'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="street_number">Street Number</label>
-                                        <input type="text" class="form-control" id="street_number" name="street_number" value="<?= $user['street_number'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="street_name">Street Name</label>
-                                        <input type="text" class="form-control" id="street_name" name="street_name" value="<?= $user['street_name'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="barangay">Barangay</label>
-                                        <input type="text" class="form-control" id="barangay" name="barangay" value="<?= $user['barangay'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="city">City</label>
-                                        <input type="text" class="form-control" id="city" name="city" value="<?= $user['city'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="region">Region</label>
-                                        <input type="text" class="form-control" id="region" name="region" value="<?= $user['region'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="zip_code">Zip Code</label>
-                                        <input type="text" class="form-control" id="zip_code" name="zip_code" value="<?= $user['zip_code'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="email">Email</label>
-                                        <input type="email" class="form-control" id="email" name="email_address" value="<?= $user['email_address'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="mobile">Mobile Number</label>
-                                        <input type="text" class="form-control" id="mobile" name="mobile_number" value="<?= $user['mobile_number'] ?>" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="sex">Sex</label>
-                                        <select class="form-control" id="sex" name="sex">
+            <!-- Update Profile Button -->
+            <!-- Update Profile Button -->
+            <div class="row" style="display:block; text-align: right; margin:2px;">
+                <a class="btn btn-success btn-sm col-sm-2" href="#" style="margin:10px; margin-left:0px;" data-toggle="modal" data-target="#updateProfileModal">Update Profile</a>
+            </div>
+
+            <!-- Modal for Updating Profile -->
+            <div class="modal fade" id="updateProfileModal" tabindex="-1" role="dialog" aria-labelledby="updateProfileModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document"> <!-- Added modal-lg class here -->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="updateProfileModalLabel">Update Profile</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <!-- User Information Form -->
+                            <form id="updateProfileForm">
+                                <div class="form-group">
+                                    <label for="profilePhoto">Profile Photo</label>
+                                    <input type="file" class="form-control-file" id="profilePhoto" name="photo">
+                                </div>
+                                <div class="form-group">
+                                    <label for="firstName">First Name</label>
+                                    <input type="text" class="form-control" id="firstName" name="first_name" value="<?= $user['first_name'] ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="middleName">Middle Name</label>
+                                    <input type="text" class="form-control" id="middleName" name="middle_name" value="<?= $user['middle_name'] ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="lastName">Last Name</label>
+                                    <input type="text" class="form-control" id="lastName" name="last_name" value="<?= $user['last_name'] ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="street_number">Street Number</label>
+                                    <input type="text" class="form-control" id="street_number" name="street_number" value="<?= $user['street_number'] ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="street_name">Street Name</label>
+                                    <input type="text" class="form-control" id="street_name" name="street_name" value="<?= $user['street_name'] ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="barangay">Barangay</label>
+                                    <input type="text" class="form-control" id="barangay" name="barangay" value="<?= $user['barangay'] ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="city">City</label>
+                                    <input type="text" class="form-control" id="city" name="city" value="<?= $user['city'] ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="region">Region</label>
+                                    <input type="text" class="form-control" id="region" name="region" value="<?= $user['region'] ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="zip_code">Zip Code</label>
+                                    <input type="text" class="form-control" id="zip_code" name="zip_code" value="<?= $user['zip_code'] ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email_address" value="<?= $user['email_address'] ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="mobile">Mobile Number</label>
+                                    <input type="text" class="form-control" id="mobile" name="mobile_number" value="<?= $user['mobile_number'] ?>" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="sex">Sex</label>
+                                    <select class="form-control" id="sex" name="sex">
                                         <option value="Male" <?= $user['sex'] == 'Male' ? 'selected' : '' ?>>Male</option>
                                         <option value="Female" <?= $user['sex'] == 'Female' ? 'selected' : '' ?>>Female</option>
                                         <option value="Other" <?= $user['sex'] == 'Other' ? 'selected' : '' ?>>Other</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="maritalStatus">Marital Status</label>
-                                        <select class="form-control" id="maritalStatus" name="marital_status">
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="maritalStatus">Marital Status</label>
+                                    <select class="form-control" id="maritalStatus" name="marital_status">
                                         <option value="Single" <?= $user['marital_status'] == 'Single' ? 'selected' : '' ?>>Single</option>
                                         <option value="Married" <?= $user['marital_status'] == 'Married' ? 'selected' : '' ?>>Married</option>
                                         <option value="Divorced" <?= $user['marital_status'] == 'Divorced' ? 'selected' : '' ?>>Divorced</option>
-                                        </select>
-                                    </div>
-                                    <!-- Trainer Profile Form -->
-                                    <div class="form-group">
-                                        <label for="keyCompetencies">Key Competencies</label>
-                                        <textarea class="form-control" id="keyCompetencies" name="key_competencies" rows="3"><?= $trainer['key_competencies'] ?></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="educationalBackground">Educational Background</label>
-                                        <textarea class="form-control" id="educationalBackground" name="educational_background" rows="3"><?= $trainer['educational_background'] ?></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="employmentHistory">Employment History</label>
-                                        <textarea class="form-control" id="employmentHistory" name="employment_history" rows="3"><?= $trainer['employment_history'] ?></textarea>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary" id="updateProfileSubmitBtn">Save changes</button>
-                            </div>
+                                    </select>
+                                </div>
+
+                                <!-- Trainer Profile Form -->
+                                <div class="form-group">
+                                    <label for="keyCompetencies">Key Competencies</label>
+                                    <textarea class="form-control" id="keyCompetencies" name="key_competencies" rows="3"><?= $trainer['key_competencies'] ?></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="educationalBackground">Educational Background</label>
+                                    <textarea class="form-control" id="educationalBackground" name="educational_background" rows="3"><?= $trainer['educational_background'] ?></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="employmentHistory">Employment History</label>
+                                    <textarea class="form-control" id="employmentHistory" name="employment_history" rows="3"><?= $trainer['employment_history'] ?></textarea>
+                                </div>
+                             
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" form="updateProfileForm" class="btn btn-primary">Save Changes</button>
                         </div>
                     </div>
                 </div>
+            </div>
 
+
+
+                <!-- User Info -->
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">Personal Information</h3>
@@ -310,11 +316,10 @@
                         <?php endif; ?>
                     </div>
                 </div>
-                <!-- /.row -->
-            </div><!-- /.container-fluid -->
+            </div>
         </div>
-        <!-- /.content -->
-    </div>
+    </div>                       
+
     <!-- /.content-wrapper -->
     <!-- Control Sidebar -->
 
@@ -343,22 +348,39 @@
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo base_url() . '/assets/template/dist' ?>/js/demo.js"></script>
 <script>
-    $(function () {
-        $('.select2').select2({
-            maximumSelectionLength: 2,
-            formatSelectionTooBig: function (limit) {
-                return 'Too many collaborators';
+$(document).ready(function() {
+    // Handle form submission
+    $('#updateProfileForm').on('submit', function(event) {
+        event.preventDefault();
+
+        var formData = new FormData(this);
+
+        $.ajax({
+            url: '<?= base_url('trainer/update_profile') ?>',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                var data = JSON.parse(response);
+                if (data.status === 'success') {
+                    alert(data.message);
+                    // Close the modal
+                    $('#updateProfileModal').modal('hide');
+                    // Optionally, you can reload the page to reflect the changes
+                    location.reload();
+                } else {
+                    alert('Error: ' + data.message);
+                }
+            },
+            error: function() {
+                alert('There was an error processing your request.');
             }
         });
-        $('[data-toggles="tooltip"]').tooltip();
-        jQuery('#example1 tbody').on('click', '.add-collaborator-btn', function () {
-            jQuery('#training-id').val(jQuery(this).data('id'));
-        });
-        $("#example1").DataTable({
-            "responsive": true,
-            "autoWidth": false,
-        });
     });
+});
+
+
 </script>
 </body>
 </html>
