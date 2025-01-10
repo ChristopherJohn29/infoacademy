26fd669bf78faa1fe46d95492e08cb26287deaa9
Christopher John Gamo <christopherjohngamo@gmail.com>
2025-01-11 00:05:01 +0800

    update

--- application/views/trainer/profile.php
+++ application/views/trainer/profile.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Trainer | Dashboard</title>
+    <title>Trainer | My Profile</title>
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="<?php echo base_url() . '/assets/template/plugins' ?>/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
