994a62b52918ef488242d2df6e3985c4b598f054
Christopher John Gamo <christopherjohngamo@gmail.com>
2025-01-11 00:09:18 +0800

    update

--- application/views/trainer/dashboard.php
+++ application/views/trainer/dashboard.php
@@ -194,11 +194,7 @@
                     <div class="col-sm-6">
                         <h1 class="m-0 text-dark"> My Dashboard</h1>
                     </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item active">Home</li>
-                        </ol>
-                    </div><!-- /.col -->
+        
                 </div><!-- /.row -->
             </div><!-- /.container-fluid -->
         </div>
