8b15b5ff28e01fe6576d8faaf118d464639dcec7
Christopher John Gamo <christopherjohngamo@gmail.com>
2025-01-14 23:01:07 +0800

    update

--- application/models/System_model.php
+++ application/models/System_model.php
@@ -29,7 +29,7 @@ class System_model extends CI_Model
     public function fetchTrainingByAuthorAndID($author = false,$id = false)
     {
         $this->db->select('*');
-        $this->db->where('author_id', $id);
+        $this->db->where('author_id', $author);
         $this->db->where('id', $id);
         $this->db->where('status', 1);
         $this->db->from('training');
