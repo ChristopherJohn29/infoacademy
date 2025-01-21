126f66fe49f8586ca88b68ef433a413910792e53
christophergamo29 <christopherjohngamo@gmail.com>
2025-01-22 00:10:05 +0800

    update

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -1,99 +1,55 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
-             with font-awesome or any other icon font library -->
-
+        <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+        
         <li class="nav-item">
-            <?php
-            if ($url == base_url() . 'admin/trainings') {
-                $active = 'active';
-            } else {
-                $active = '';
-            }
-            ?>
-            <a href="<?= base_url() . 'admin/trainings' ?>" class="nav-link <?= $active ?>">
+            <a href="<?= base_url('admin/trainings') ?>" class="nav-link <?= $this->uri->segment(2) === 'trainings' ? 'active' : '' ?>">
                 <i class="nav-icon fas fa-chalkboard-teacher"></i>
-                <p>
-                    Trainings </p>
+                <p>Trainings</p>
             </a>
         </li>
+        
         <li class="nav-item">
-            <?php
-            if ($url == base_url() . 'admin/payments') {
-                $active = 'active';
-            } else {
-                $active = '';
-            }
-            ?>
-            <a href="<?= base_url() . 'admin/payments' ?>" class="nav-link <?= $active ?>">
+            <a href="<?= base_url('admin/payments') ?>" class="nav-link <?= $this->uri->segment(2) === 'payments' ? 'active' : '' ?>">
                 <i class="nav-icon fas fa-wallet"></i>
-                <p>
-                    Payments </p>
+                <p>Payments</p>
             </a>
         </li>
+        
         <li class="nav-item">
-            <?php
-            if ($url == base_url() . 'admin/coupons') {
-                $active = 'active';
-            } else {
-                $active = '';
-            }
-            ?>
-            <a href="<?= base_url() . 'admin/coupons' ?>" class="nav-link <?= $active ?>">
+            <a href="<?= base_url('admin/coupons') ?>" class="nav-link <?= $this->uri->segment(2) === 'coupons' ? 'active' : '' ?>">
                 <i class="nav-icon fa fa-gift"></i>
-                <p>
-                    Coupons </p>
+                <p>Coupons</p>
             </a>
         </li>
+        
         <li class="nav-item">
-            <?php
-            if ($url == base_url() . 'admin/category') {
-                $active = 'active';
-            } else {
-                $active = '';
-            }
-            ?>
-            <a href="<?= base_url() . 'admin/category' ?>" class="nav-link <?= $active ?>">
+            <a href="<?= base_url('admin/category') ?>" class="nav-link <?= $this->uri->segment(2) === 'category' ? 'active' : '' ?>">
                 <i class="nav-icon fa fa-list"></i>
-                <p>
-                    Category </p>
+                <p>Category</p>
             </a>
         </li>
+        
         <li class="nav-item">
-            <?php
-            if ($url == base_url() . 'admin/trainer') {
-                $active = 'active';
-            } else {
-                $active = '';
-            }
-            ?>
-            <a href="<?= base_url() . 'admin/trainer' ?>" class="nav-link <?= $active ?>">
+            <a href="<?= base_url('admin/trainer') ?>" class="nav-link <?= $this->uri->segment(2) === 'trainer' ? 'active' : '' ?>">
                 <i class="nav-icon fa fa-users"></i>
-                <p>
-                    Trainer </p>
+                <p>Trainer</p>
             </a>
         </li>
+        
         <li class="nav-item">
-            <?php
-            if ($url == base_url() . 'admin/systemControl') {
-                $active = 'active';
-            } else {
-                $active = '';
-            }
-            ?>
-            <a href="<?= base_url() . 'admin/systemControl' ?>" class="nav-link <?= $active ?>">
+            <a href="<?= base_url('admin/systemControl') ?>" class="nav-link <?= $this->uri->segment(2) === 'systemControl' ? 'active' : '' ?>">
                 <i class="nav-icon fa fa-cog"></i>
-                <p>
-                    System Control </p>
+                <p>System Control</p>
             </a>
         </li>
-
+        
         <li class="nav-item">
-            <a href="<?= base_url() . 'control/logout' ?>" class="nav-link">
-                <i class="nav-icon fa fa-sign-out"></i>
-                <p>
-                    Logout </p>
+            <a href="<?= base_url('control/logout') ?>" class="nav-link">
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p>Logout</p>
             </a>
         </li>
     </ul>
+
 </nav>
