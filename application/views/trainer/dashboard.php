cf226ad188177269139fdd77bf6aee87c50e29fb
christophergamo29 <christopherjohngamo@gmail.com>
2025-01-30 21:25:52 +0800

    update

--- application/views/trainer/dashboard.php
+++ application/views/trainer/dashboard.php
@@ -119,12 +119,15 @@
                 </div>
 
                 <!-- Message Modal -->
-                <div class="modal fade" id="messageModal" tabindex="-1" aria-labelledby="messageModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
+                <!-- Message Modal -->
+                <div class="modal fade" id="messageModal" tabindex="-1" role="dialog" aria-labelledby="messageModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="messageModalLabel">Message Enrollees</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
                             </div>
                             <div class="modal-body">
                                 <div id="messageContainer" class="message-container">
@@ -133,12 +136,13 @@
                                 <textarea id="messageContent" class="form-control" rows="4" placeholder="Type your message here..."></textarea>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                 <button type="button" class="btn btn-primary" onclick="sendMessage()">Send</button>
                             </div>
                         </div>
                     </div>
                 </div>
+
 
               
                 <div class="row">
