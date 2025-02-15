69abd47e12b2019316ce0e69e2a8b6864fd23d2d
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-15 20:44:55 +0800

    update

--- application/views/training_single.php
+++ application/views/training_single.php
@@ -113,7 +113,7 @@
                 </div>
                 <!-- Course Sidebar -->
                 <div class="col-lg-4">
-                    <div class="sidebar" style="padding-top: 0px;">
+                    <div class="sidebar" style="padding-top: 21px;">
                         <!-- Feature -->
                         <div class="sidebar_section">
                             <div class="sidebar_section_title">Training Feature</div>
