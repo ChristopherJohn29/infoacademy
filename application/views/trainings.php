121834ded89a4c8d4443ae1d1b02a2ec70e24547
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-15 22:03:26 +0800

    update

--- application/views/trainings.php
+++ application/views/trainings.php
@@ -124,7 +124,50 @@
                             </div>
                         </div>
                         <!-- Latest Course -->
-         
+                        <div class="sidebar_section" style="display:none;">
+                            <div class="sidebar_section_title">Latest Trainings</div>
+                            <div class="sidebar_latest">
+                                <!-- Latest Course -->
+                                <div class="latest d-flex flex-row align-items-start justify-content-start">
+                                    <div class="latest_image">
+                                        <div>
+                                            <img src="<?php echo base_url() . '/assets/unicat/images' ?>/latest_1.jpg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="latest_content">
+                                        <div class="latest_title">
+                                            <a href="course.html">How to Design a Logo a Beginners Course</a></div>
+                                        <div class="latest_price">Free</div>
+                                    </div>
+                                </div>
+                                <!-- Latest Course -->
+                                <div class="latest d-flex flex-row align-items-start justify-content-start">
+                                    <div class="latest_image">
+                                        <div>
+                                            <img src="<?php echo base_url() . '/assets/unicat/images' ?>/latest_2.jpg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="latest_content">
+                                        <div class="latest_title">
+                                            <a href="course.html">Photography for Beginners Masterclass</a></div>
+                                        <div class="latest_price">₱170</div>
+                                    </div>
+                                </div>
+                                <!-- Latest Course -->
+                                <div class="latest d-flex flex-row align-items-start justify-content-start">
+                                    <div class="latest_image">
+                                        <div>
+                                            <img src="<?php echo base_url() . '/assets/unicat/images' ?>/latest_3.jpg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="latest_content">
+                                        <div class="latest_title"><a href="course.html">The Secrets of Body Language</a>
+                                        </div>
+                                        <div class="latest_price">₱220</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <!-- Gallery -->
                         <!-- Tags -->
                         <!-- Banner -->
