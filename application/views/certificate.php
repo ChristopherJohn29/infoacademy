52d47a355c56a07ba583138b4539b5b084ae12dd
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-19 05:49:32 +0800

    updatE

--- application/views/certificate.php
+++ application/views/certificate.php
@@ -42,7 +42,7 @@
       font-weight: bolder;
       margin-bottom: 6.5rem;
       margin-top: 14.5rem;
-      margin-left: 5rem;
+      margin-left: 7rem;
     }
 
     .course-title {
@@ -50,7 +50,7 @@
       font-weight: bold;
       margin-bottom: 1.5rem;
       line-height: 1.4;
-      margin-left: 5rem;
+      margin-left: 9rem;
     }
 
     .completion-info {
