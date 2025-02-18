85d2e74af3b0b0f05c0ff9b496dbb0734e933474
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-19 06:48:27 +0800

    updatE

--- application/config/email.php
+++ application/config/email.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['wordwrap'] = TRUE;
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = 'smtp.gmail.com';
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
 $config['smtp_user'] = 'konozubadoh@gmail.com'; // Your email username
 $config['smtp_pass'] = 'dacdznqsvhxgqclp'; // Your email password
 $config['smtp_port'] = '465';
