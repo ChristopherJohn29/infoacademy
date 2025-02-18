6730fadd9de5cbd413690297406dc32146492350
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-19 05:41:26 +0800

    updatE

--- application/views/certificate.php
+++ application/views/certificate.php
@@ -25,11 +25,6 @@
       width: 297mm;
       height: 210mm;
       position: relative;
-
-      /* Option A: Use a file:// path if you prefer local filesystem references:
-         background: url("file:///<?= FCPATH . 'assets/unicat/images/certificate.png' ?>") no-repeat center center; */
-
-      /* Option B: Use base_url() if you have isRemoteEnabled = true in Dompdf: */
       background: url("<?= base_url('assets/unicat/images/certificate.png') ?>") no-repeat center center;
 
       background-size: cover;
