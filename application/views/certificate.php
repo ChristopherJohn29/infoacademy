46ba53ce145d1836c52cfba9cf5b3c0ca02b3b94
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-19 05:44:20 +0800

    updatE

--- application/views/certificate.php
+++ application/views/certificate.php
@@ -23,7 +23,13 @@
     /* A4 landscape dimensions: 297mm x 210mm */
     .certificate-bg {
       width: 297mm;
+      height: 210mm;
       position: relative;
+
+      /* Option A: Use a file:// path if you prefer local filesystem references:
+         background: url("file:///<?= FCPATH . 'assets/unicat/images/certificate.png' ?>") no-repeat center center; */
+
+      /* Option B: Use base_url() if you have isRemoteEnabled = true in Dompdf: */
       background: url("<?= base_url('assets/unicat/images/certificate.png') ?>") no-repeat center center;
 
       background-size: cover;
