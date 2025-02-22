b3f63fecc3041264d1d1ad0a503d0507e3abcc50
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-22 23:33:05 +0800

    update

--- application/controllers/Notification.php
+++ application/controllers/Notification.php
@@ -3,7 +3,7 @@ class Notification extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model('Notification_model');
+        $this->load->model('notification_model');
         // Ensure your session library is loaded if you use session-based authentication
         $this->load->library('session');
     }
@@ -11,13 +11,13 @@ class Notification extends CI_Controller {
     // Returns notifications as JSON (for AJAX calls)
     public function get_notifications() {
         $user_id = $this->session->userdata('user_id'); // adjust according to your auth system
-        $notifications = $this->Notification_model->get_notifications($user_id);
+        $notifications = $this->notification_model->get_notifications($user_id);
         echo json_encode($notifications);
     }
 
     // Mark a notification as read (triggered via user action)
     public function mark_read($id) {
-        $this->Notification_model->mark_as_read($id);
+        $this->notification_model->mark_as_read($id);
         echo json_encode(['status' => 'success']);
     }
 }
