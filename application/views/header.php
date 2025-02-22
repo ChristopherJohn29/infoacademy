00a711f136e38e40bf7e253efe05eed97df138c8
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-23 00:13:44 +0800

    notificatiom

--- application/views/header.php
+++ application/views/header.php
@@ -1,3 +1,25 @@
+<style>
+
+.notification-module .notification-link {
+  position: relative;
+  color: #333; /* or your theme color */
+}
+.notification-module .notification-badge {
+  background: #f39c12;
+  color: #fff;
+  padding: 2px 6px;
+  border-radius: 50%;
+  font-size: 0.75rem;
+  position: absolute;
+  top: -5px;
+  right: -10px;
+}
+.notification-dropdown {
+  width: 300px;
+}
+
+</style>
+
 <header class="header">
     <!-- Top Bar -->
     <div class="top_bar">
@@ -34,6 +56,35 @@
                             </div>
 
                         </div>
+
+                        <!-- Notification Module for Unicat Template -->
+                        <div class="notification-module dropdown ml-auto">
+                        <a href="#" class="notification-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-bell" aria-hidden="true"></i>
+                            <span class="notification-badge">15</span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right notification-dropdown">
+                            <h6 class="dropdown-header">15 Notifications</h6>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">
+                            <i class="fa fa-envelope mr-2"></i> 4 new messages
+                            <span class="float-right text-muted text-sm">3 mins</span>
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">
+                            <i class="fa fa-users mr-2"></i> 8 friend requests
+                            <span class="float-right text-muted text-sm">12 hours</span>
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">
+                            <i class="fa fa-file mr-2"></i> 3 new reports
+                            <span class="float-right text-muted text-sm">2 days</span>
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item text-center" href="#">See All Notifications</a>
+                        </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
