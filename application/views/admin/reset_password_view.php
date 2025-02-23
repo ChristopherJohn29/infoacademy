9d0df772b6fedaad186006346365cf6795bdce84
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-23 16:45:29 +0800

    update

--- application/views/admin/reset_password_view.php
+++ application/views/admin/reset_password_view.php
@@ -16,6 +16,8 @@
   <link rel="stylesheet" href="<?php echo base_url('assets/template/dist/css/adminlte.min.css'); ?>">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url().'/assets/unicat/styles/'?>main_styles.css">
   <style>
     body {
       color: black;
