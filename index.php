c6b229f5dc0574bbb9b13970095b46ec46b2a0cb
christophergamo29 <christopherjohngamo@gmail.com>
2025-02-23 13:35:36 +0800

    update

--- index.php
+++ index.php
@@ -63,8 +63,8 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
-ini_set('display_errors', 1);
+error_reporting(0);
+ini_set('display_errors', 0);
 /*
  *---------------------------------------------------------------
  * SYSTEM DIRECTORY NAME
