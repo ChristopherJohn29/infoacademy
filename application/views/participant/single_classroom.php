1164ac6d95866f143199440413db011786a028d9
christophergamo29 <christopherjohngamo@gmail.com>
2025-03-23 21:44:34 +0800

    update

--- application/views/participant/single_classroom.php
+++ application/views/participant/single_classroom.php
@@ -150,6 +150,7 @@
                 <?php
                 $training_class = $this->System_model->fetchFromTrainingClass($_GET['tid']);
                 $instruction = json_decode($training_class[0]['training_instruction']);
+                $references = json_decode($training_class[0]['training_reference']);
 
                 ?>
                 <div class="col-md-12">
@@ -181,6 +182,36 @@
                         </div>
                         <!-- /.card-body -->
                     </div>
+
+                    <div class="card card-warning">
+                        <div class="card-header">
+                            <h3 class="card-title">Training References and Example</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <?php
+                            $count = 0;
+                            foreach ($references as $value) {
+                                $count++;
+                                ?>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <!-- text input -->
+                                        <div class="form-group">
+                                            <label><?= $count ?>: <a target="_blank" href="<?= $value->reference_url ?>"><?= $value->reference_title ?></a></label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                            }
+
+                            ?>
+                            <!-- input states -->
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+
+
                     <?php
 
                     $count = 0;
